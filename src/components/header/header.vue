5915ca25eeb5bd47433bbccf2e90758ec0d78f3b
tanbiqin <939547533@qq.com>
2017-06-27 11:43:49 +0800

    add seller-header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,35 +13,174 @@
 					{{seller.description}}/{{seller.deliveryTime}}分钟送达
 				</div>
 				<div v-if="seller.supports" class="support">
-					<span class="icon"></span>
+					<span class="icon" :class="classMap[seller.supports[0].type]"></span>
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div v-if="seller.supports" class="support-count" @click="showDetail()">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-thumb_up"></i>
+			</div>
 		</div>
-		<div class="bulletin-wrapper">
-			
+		<div class="bulletin-wrapper" @click="showDetail()">
+			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class="icon-thumb_up"></i>
+		</div>
+		<div class="background">
+			<img width="100%" height="100%" :src="seller.avatar" alt="商家背景图">
+		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper clearfix">
+				<div class="detail-main"></div>
+				<div class="detail-close"></div>
+			</div>
 		</div>
 	</div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
+		},
+		data() {
+			return {
+				detailShow: false
+			};
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true;
+			}
+		},
+		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 		}
 	};
 </script>
 
 <style rel="stylesheet/stylus" lang="stylus" scoped>
+	@import "../../common/stylus/mixin";
 	.header
+		position: relative
+		overflow: hidden
 		color: #FFF
-		background #000
+		background: rgba(7, 17, 27, 0.5)
 		.content-wrapper
+			position: relative
 			padding: 24px 12px 18px 24px 
+			font-size: 0
 			.avatar
 				display: inline-block
+				vertical-align: top
+				img
+					border-radius: 2px
 			.content
 				display: inline-block
+				margin-left: 16px
+				font-size: 14px
+				.title
+					margin: 2px 0 8px 0
+					.brand
+						display: inline-block
+						vertical-align: top
+						width: 30px
+						height: 18px
+						bg-image('brand')
+						background-size: 30px 18px
+						background-repeat: no-repeat
+					.name
+						margin-left: 6px
+						font-size: 16px
+						line-height: 18px
+						font-weight: bold
+				.description
+					margin-bottom: 10px
+					line-height: 12px
+					font-size: 12px
+				.support
+					font-size: 0
+					.icon
+						display: inline-block
+						vertical-align: top
+						width: 12px 
+						height: 12px
+						margin-right: 4px
+						background-size: 12px 12px
+						background-repeat: no-repeat
+						&.decrease
+							bg-image('decrease_1')
+						&.discount
+							bg-image('discount_1')
+						&.guarantee
+							bg-image('guarantee_1')
+						&.invoice
+							bg-image('invoice_1')
+						&.special
+							bg-image('special_1')	
+					.text
+						line-height:12px
+						font-size: 10px
+			.support-count
+				position: absolute
+				right: 12px
+				bottom: 18px
+				padding: 0 8px 
+				height: 24px
+				line-height: 24px
+				border-radius: 14px
+				background:rgba(0, 0, 0, 0.2)
+				text-align: center
+				.count
+					vertical-align: top
+					font-size: 10px
+				.icon-thumb_up
+					line-height: 24px
+					margin-left: 2px
+					font-size: 10px
+		.bulletin-wrapper
+			position: relative
+			height: 28px
+			line-height: 28px
+			padding: 0 22px 0 12px
+			white-space:nowrap
+			overflow: hidden
+			text-overflow: ellipsis
+			background: rgba(7, 17, 27, 0.2)
+			.bulletin-title
+				display: inline-block
+				width: 22px
+				height: 12px
+				bg-image('bulletin')
+				background-size: 22px 12px
+				background-repeat: no-repeat
+			.bulletin-text
+				vertical-align: top
+				font-size: 10px
+				margin: 0 4px
+			.icon-thumb_up
+				position: absolute
+				font-size: 10px
+				right: 12px
+				top: 8px	
+		.background
+			position: absolute
+			width: 100%
+			height: 100%
+			top: 30px
+			left: 0
+			z-index: -1
+			filter: blur(10px)
+		.detail
+			position: fixed
+			width: 100%
+			height: 100%
+			top: 0
+			left: 0
+			z-index: 100
+			overflow: auto
+			background: rgba(7, 17, 27, 0.8)
+			
 </style>
