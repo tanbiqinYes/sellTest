1e7783bce83e2ff4d765d6005bf6459f597b6f79
tanbiqin <939547533@qq.com>
2017-06-28 18:13:28 +0800

    add header -star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -19,26 +19,46 @@
 			</div>
 			<div v-if="seller.supports" class="support-count" @click="showDetail()">
 				<span class="count">{{seller.supports.length}}个</span>
-				<i class="icon-thumb_up"></i>
+				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
 		<div class="bulletin-wrapper" @click="showDetail()">
 			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
-			<i class="icon-thumb_up"></i>
+			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 		<div class="background">
 			<img width="100%" height="100%" :src="seller.avatar" alt="商家背景图">
 		</div>
 		<div v-show="detailShow" class="detail">
 			<div class="detail-wrapper clearfix">
-				<div class="detail-main"></div>
-				<div class="detail-close"></div>
+				<div class="detail-main">
+					<h1 class="name">{{seller.name}}</h1>
+					<div class="star-wapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<ul v-if="seller.supports" class="supports">
+						<li class="support-item" v-for="item in seller.supports">
+							<span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+							<span class="text">{{seller.supports[$index].description}}</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close"></i>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import star from '../../components/star/star.vue';
+
 	export default {
 		props: {
 			seller: {
@@ -57,6 +77,9 @@
 		},
 		created() {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		},
+		components: {
+			star
 		}
 	};
 </script>
@@ -136,7 +159,7 @@
 				.count
 					vertical-align: top
 					font-size: 10px
-				.icon-thumb_up
+				.icon-keyboard_arrow_right
 					line-height: 24px
 					margin-left: 2px
 					font-size: 10px
@@ -160,7 +183,7 @@
 				vertical-align: top
 				font-size: 10px
 				margin: 0 4px
-			.icon-thumb_up
+			.icon-keyboard_arrow_right
 				position: absolute
 				font-size: 10px
 				right: 12px
@@ -175,12 +198,76 @@
 			filter: blur(10px)
 		.detail
 			position: fixed
-			width: 100%
-			height: 100%
 			top: 0
 			left: 0
+			width: 100%
+			height: 100%
 			z-index: 100
 			overflow: auto
 			background: rgba(7, 17, 27, 0.8)
-			
+			.detail-wrapper
+				min-height: 100%
+				width: 100%
+				.detail-main
+					margin-top: 64px
+					padding-bottom:64px
+					.name
+						line-height: 16px
+						text-align: center
+						font-size: 16px
+						font-weight: 700
+					.star-wapper
+						margin-top: 18px 
+						padding: 2px 0
+						text-align: center
+					.title
+						display: flex
+						width: 80%
+						margin: 28px auto 24px
+						.line
+							flex: 1
+							position: relative
+							top: -6px
+							border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+						.text
+							padding:0 12px
+							font-weight: 700
+							font-size: 14px
+					.supports
+						width: 80%
+						margin: 0 auto
+						.support-item
+							padding: 0 12px
+							margin-bottom: 12px
+							font-size: 0
+							&:last-child
+								margin-bottom: 0
+							.icon
+								display: inline-block
+								width: 16px
+								height: 16px
+								vertical-align: top
+								background-size: 16px 16px
+								background-repeat: no-repeat
+								&.decrease
+									bg-image('decrease_2')
+								&.discount
+									bg-image('discount_2')
+								&.guarantee
+									bg-image('guarantee_2')
+								&.invoice
+									bg-image('invoice_2')
+								&.special
+									bg-image('special_2')
+							.text
+								line-height: 12px
+								font-size: 12px	
+			.detail-close
+				position: relative
+				width: 32px
+				height: 32px
+				margin: -64px auto 0 auto
+				clear: both
+				font-size: 32px
+					
 </style>
